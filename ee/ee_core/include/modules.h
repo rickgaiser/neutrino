0ac1a16a6c0f6f7860f5e8fd76e91aecbe17e323
Rick Gaiser <rgaiser@gmail.com>
2023-07-22 22:42:19 +0200

    Add header include guards

--- ee/ee_core/include/modules.h
+++ ee/ee_core/include/modules.h
@@ -1,3 +1,6 @@
+#ifndef MODULES_H
+#define MODULES_H
+
 enum EECORE_MODULE_ID {
     EECORE_MODULE_ID_IOPRP = 1,
     EECORE_MODULE_ID_CDVDMAN,
@@ -24,3 +27,5 @@ typedef struct
 #define GET_OPL_MOD_ID(x)   ((x) >> 24)
 #define SET_OPL_MOD_ID(x)   ((x) << 24)
 #define GET_OPL_MOD_SIZE(x) ((x)&0x00FFFFFF)
+
+#endif
