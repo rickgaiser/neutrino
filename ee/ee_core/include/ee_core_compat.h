0045756e31cd7e94155f5f8a4d4d91c3bf4002de
Maximus32 <rgaiser@gmail.com>
2024-11-29 21:50:01 +0100

    Fixup, forgot 1 file.

---
+++ ee/ee_core/include/ee_core_compat.h
@@ -0,0 +1,9 @@
+#ifndef EE_CORE_COMPAT_H
+#define EE_CORE_COMPAT_H
+
+// This file is also used by assembly (.S) code
+// So keep it simple
+
+#define EECORE_COMPAT_UNHOOK (1<<0) // Unhook syscalls
+
+#endif
