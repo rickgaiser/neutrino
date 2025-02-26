a16a032cf25105d351d4882cc6a8074e1447ea1c
Maximus32 <rgaiser@gmail.com>
2025-02-26 19:32:27 +0100

    ee_core: cleanup

--- ee/ee_core/include/ee_core_flag.h
+++ ee/ee_core/include/ee_core_flag.h
@@ -4,13 +4,14 @@
 // This file is also used by assembly (.S) code
 // So keep it simple
 
-#define EECORE_FLAG_UNHOOK      (1<<0) // Unhook syscalls
-#define EECORE_FLAG_GSM_FLD_FP  (1<<1) // GSM: Field Mode: Force Progressive
-#define EECORE_FLAG_GSM_FRM_FP1 (1<<2) // GSM: Frame Mode: Force Progressive (240p)
-#define EECORE_FLAG_GSM_FRM_FP2 (1<<3) // GSM: Frame Mode: Force Progressive (line-double)
-#define EECORE_FLAG_GSM_NO_576P (1<<4) // GSM: Disable GSM 576p mode
-#define EECORE_FLAG_GSM_C_1     (1<<5) // GSM: Enable FIELD flip type 1
-#define EECORE_FLAG_GSM_C_2     (1<<6) // GSM: Enable FIELD flip type 2
-#define EECORE_FLAG_GSM_C_3     (1<<7) // GSM: Enable FIELD flip type 3
+#define EECORE_FLAG_UNHOOK      (1<< 0) // Unhook syscalls
+
+#define EECORE_FLAG_GSM_FLD_FP  (1<<10) // GSM: Field Mode: Force Progressive
+#define EECORE_FLAG_GSM_FRM_FP1 (1<<11) // GSM: Frame Mode: Force Progressive (240p)
+#define EECORE_FLAG_GSM_FRM_FP2 (1<<12) // GSM: Frame Mode: Force Progressive (line-double)
+#define EECORE_FLAG_GSM_NO_576P (1<<13) // GSM: Disable GSM 576p mode
+#define EECORE_FLAG_GSM_C_1     (1<<14) // GSM: Enable FIELD flip type 1
+#define EECORE_FLAG_GSM_C_2     (1<<15) // GSM: Enable FIELD flip type 2
+#define EECORE_FLAG_GSM_C_3     (1<<16) // GSM: Enable FIELD flip type 3
 
 #endif
