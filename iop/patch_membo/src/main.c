f67a1aeeb284016b613240ada0479b5c850666fe
Maximus32 <rgaiser@gmail.com>
2025-06-14 11:32:59 +0200

    patch_membo: add parameter for choosing the size

--- iop/patch_membo/src/main.c
+++ iop/patch_membo/src/main.c
@@ -11,19 +11,26 @@ IRX_ID(MODNAME, 1, 1);
  * membo = Memory Buffer Overrun
  * -----------------------------
  * This module tries to fix games that have a buffer overrun issue.
- * By allocating 256 bytes (the smallest amount) extra.
+ * By allocating 256 bytes (the smallest amount) or more extra.
  */
 
 typedef void * (*fp_AllocSysMemory)(int mode, int size, void *ptr);
 fp_AllocSysMemory org_AllocSysMemory;
+int extra_size = 256;
 
 static void * hooked_AllocSysMemory(int mode, int size, void *ptr)
 {
-    return org_AllocSysMemory(mode, size + 256, ptr);
+    return org_AllocSysMemory(mode, size + extra_size, ptr);
 }
 
 int _start(int argc, char **argv)
 {
+    if (argc >= 1) {
+        char c = argv[1][0];
+        if (c >= '0' && c <= '9')
+            extra_size = 256 << (c - '0');
+    }
+
     // Hook sysmem functions
     iop_library_t * lib_sysmem = ioplib_getByName("sysmem");
     org_AllocSysMemory = ioplib_hookExportEntry(lib_sysmem, 4, hooked_AllocSysMemory);
